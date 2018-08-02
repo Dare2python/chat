52baf83f7ff49bb295a6dc507462e56233eee868
Dare2python <dare2python@gmail.com>
2018-08-02 15:17:35 +0300

    Signup working

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit() {
+  }
+
+  private createForm(): void {
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  public submit(): void {
+    // TODO call the auth service
+    const {firstName, lastName, email, password} = this.signupForm.value;
+    console.log(`First Name: ${firstName}, Last Name: ${lastName}, Email: ${email}, Password: ${password}`);
   }
 
 }
